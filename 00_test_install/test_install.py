0d467ea5a802dbba7b45007be5a1dd48d3aa88b2
Nikolai Kapralov <4dvlup@gmail.com>
2025-08-31 22:02:14 +0200

    Testing the installation

---
+++ 00_test_install/test_install.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import mne
+
+from mne.datasets import sample
+
+from meegsim.location import select_random
+from meegsim.simulate import SourceSimulator
+from meegsim.waveform import narrowband_oscillation
+
+
+# Head model
+print("⬇️ Downloading and testing the template head model")
+
+data_path = sample.data_path()
+subjects_dir = data_path / "subjects"
+src = mne.read_source_spaces(subjects_dir / "fsaverage" / "bem" / "fsaverage-ico-5-src.fif")
+
+print("✅ Download complete!")
+
+
+# Basic functionality
+print("🔄 Testing basic functionality of MEEGsim")
+
+sim = SourceSimulator(src)
+sim.add_noise_sources(location=select_random, 
+                      location_params=dict(n=10))
+sim.add_point_sources(location=select_random,
+                      location_params=dict(n=3),
+                      waveform=narrowband_oscillation,
+                      waveform_params=dict(fmin=8, fmax=12))
+
+sc = sim.simulate(sfreq=250, duration=120)
+
+print("✅ Basic functionality is fine!")
+
+
+# Advanced functionality
+print("🔄 Testing advanced functionality of MEEGsim")
+
+sim = SourceSimulator(src)
+sim.add_noise_sources(location=select_random, 
+                      location_params=dict(n=10))
+sim.add_patch_sources(location=select_random,
+                      location_params=dict(n=3),
+                      waveform=narrowband_oscillation,
+                      waveform_params=dict(fmin=8, fmax=12),
+                      extents=[10, 20, 50],
+                      subject="fsaverage",
+                      subjects_dir=subjects_dir)
+
+sc = sim.simulate(sfreq=250, duration=120, random_state=1234)
+
+print("✅ Advanced functionality is fine!")
+
+
+# Plotting
+print("🔄 Testing that plotting works")
+
+brain = sc.plot(subject="fsaverage", 
+                subjects_dir=subjects_dir,
+                hemi="split", views=["lat", "med"])
+screenshot = brain.screenshot()
+brain.close()
+
+fig, ax = plt.subplots()
+ax.imshow(screenshot)
+ax.axis('off')
+
+print("✅ Plotting is fine!")
+
+
+# Happy end!
+print("✅ Everything seems to work!")
--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # Intro to MEEGsim
 
 This repository contains the materials of the workshop on how to use the MEEGsim package for simulating ground-truth M/EEG activity and connectivity patterns.
+
+## Preparing for the workshop
+
+1. [Optional, but recommended] Create an environment of your preference (
+    [conda](https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html)|
+    [venv](https://docs.python.org/3/library/venv.html)|
+    [uv](https://docs.astral.sh/uv/)|
+    etc.
+) to have no conflicts with other packages installed on your system.
+
+2. Activate the environment and install the prerequisites. If you plan to use
+Jupyter Notebook during the tutorial, use `requirements_jupyter.txt`:
+
+    ```bash
+    pip install -r requirements_jupyter.txt
+    ```
+
+    Otherwise, `requirements_minimal.txt` should suffice if you plan to work with plain Python files:
+
+    ```bash
+    pip install -r requirements_minimal.txt
+    ```
+
+3. Depending on your selection in step 2, either launch the Python script
+`00_test_install/test_install.py` or run all steps in the `00_test_install/test_install.ipynb` notebook.
+
+4. If you see "✅ Everything seems to work!" in the output, all necessary packages were installed correctly, and you're well prepared for the main part of the workshop! If any errors occurred, feel free to contact the workshop instructors.
