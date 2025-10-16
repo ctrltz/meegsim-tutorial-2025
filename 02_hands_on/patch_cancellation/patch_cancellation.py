7d501a697654177d294b90ab86d5e759177dd3d2
Nikolai Kapralov <4dvlup@gmail.com>
2025-10-16 16:13:52 +0200

    WIP patch cancellation case

---
+++ 02_hands_on/patch_cancellation/patch_cancellation.py
@@ -0,0 +1,21 @@
+def simulate_patch(hemi_idx, vertno, extent):
+    """
+    To add patch sources, use the `sim.add_patch_sources` function according to
+    its documentation:
+
+    https://meegsim.readthedocs.io/en/latest/generated/meegsim.simulate.SourceSimulator.html#meegsim.simulate.SourceSimulator.add_patch_sources
+    """
+    # FILL_ME(
+    #     "The `simulate_patch` function should return a `SourceConfiguration` " \
+    #     "object with a patch source at (hemi_idx, vertno) that has a radius of " \
+    #     "`extent` mm. Other parameters (e.g., `sfreq` or `waveform`) do not "
+    #     "play a role in this example, feel free to set them arbitrarily."
+    # )
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
